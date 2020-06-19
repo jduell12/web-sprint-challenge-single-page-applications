d5a7742dd9f1c66ba68e08bccd2180472699acf9
Jessica <duellje@oregonstate.edu>
2020-06-19 11:28:48 -0400

    having router working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import {Link, Switch, Route} from 'react-router-dom'
+import {Styledheader, Styledtitle, StyledButton} from './StyledApp'
+import Form from './components/Form'
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      <Styledheader>
+        <h2>Lambda Eats</h2>
+        <Link to={'/'}>Home</Link>
+      </Styledheader>
+      <Styledtitle>
+          <Switch>
+            <Route path='/pizza'>
+              <Form/>
+            </Route>
+
+            <Route path='/'>
+              <h1>Your favorite food, delivered while coding</h1>
+              <Link to={'/pizza'}><StyledButton>Pizza?</StyledButton></Link>
+            </Route>
+          </Switch>
+      </Styledtitle>
+    </div>
   );
 };
 export default App;
---
+++ src/StyledApp.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Styledheader = styled.div`
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 3%;
+`
+
+export const Styledtitle = styled.div`
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    flex-direction: column;
+
+`
+
+export const StyledButton = styled.button`
+    width: 300%;
+    height: 20px;
+    background-color: aliceblue;
+    
+`
